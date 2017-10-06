83d81c900b241485d764e47b8bb98c5fe7f1f26b
Nicolas Oga <2112.oga@gmail.com>
2017-10-06 16:00:27 -0300

    ✨  Accept CONCURRENT number of workers as runtime var

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
+declare required_vars="\
+  \$CONCURRENT
+  \$GITLAB_TOKEN
+  \$GITLAB_URL
+  \$NAME
+  \$WORKER_IMAGE
+"
 
-# Ensure needed vars are present
-declare -a vars=(NAME GITLAB_URL GITLAB_TOKEN WORKER_IMAGE)
+# Remove dollar sign in variable name and store all var names in an array
+IFS=' '; read -a non_prefixed_var_names <<< $(
+  echo $required_vars | sed 's/\$//g'
+)
 
-for var_name in "${vars[@]}"
+# Ensure required vars are present
+for var_name in "${non_prefixed_var_names[@]}"
 do
-  if [ -z "$(eval "echo \$$var_name")" ]; then
+  echo Testing presence of required var $var_name
+
+  if [[ ! -v $var_name ]]; then
     echo "Missing environment variable $var_name"
     exit 1
   fi
@@ -15,13 +27,12 @@ done
 mv /etc/gitlab-runner/config.toml /etc/gitlab-runner/config.toml.bu
 
 # Generate new config file from template
-cat ./tmp/config.toml.template | envsubst '\
-  \$NAME \
-  \$GITLAB_URL \
-  \$GITLAB_TOKEN \
-  \$WORKER_IMAGE \
-' \
-> /etc/gitlab-runner/config.toml
+cat ./tmp/config.toml.template \
+  | envsubst "$required_vars" \
+  > /etc/gitlab-runner/config.toml
+
+# Remove template
+rm ./tmp/config.toml.template
 
 # Run the usual entrypoint
 /usr/bin/dumb-init /entrypoint run        \
